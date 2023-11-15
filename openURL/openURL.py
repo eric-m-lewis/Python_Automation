d376e7cbe9cff53a419c8da47368863e28eb4737
eric-m-lewis <ericmatthewlewis@gmail.com>
2023-11-14 23:01:43 -0500

    Add openURL.py to repo

---
+++ openURL/openURL.py
@@ -0,0 +1,28 @@
+import webbrowser, time, os
+from datetime import datetime
+
+
+def calcTimeDiff(target, current):
+    targetTime = datetime.strptime(target, '%H:%M:%S')
+    currentTime = datetime.strptime(current, '%H:%M:%S')
+    timeRemain = targetTime - currentTime
+    timeRemainString = datetime.strptime(str(timeRemain), "%H:%M:%S")
+    timeRemainString = timeRemainString.strftime("%H:%M:%S")
+    return timeRemainString
+
+url = "https://www.twitch.tv/twitchrivals"
+targetTime = '22:00:10'
+
+currentTime = '00:00:00'
+while currentTime != targetTime:
+    os.system('cls')
+    now = datetime.now()
+    currentTime = now.strftime('%H:%M:%S')
+    timeDiffString = calcTimeDiff(targetTime, currentTime)
+    printString = f'Time Remaining: {timeDiffString}'
+    print(printString)
+    time.sleep(1)
+
+print(f"\nOpening URL: {url}")
+webbrowser.open(url)
+print(currentTime)
